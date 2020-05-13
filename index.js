7488a8504803ce439299bc110784ec494d24aabe
Vitche Research Team Developer <developer@vitche.com>
2020-05-13 09:26:46 -0400

    1. Added fields for "HyperPerson" use cases.

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 module.exports = {
     User: mongoose.model('User', new mongoose.Schema({
         logOn: 'string',
-        password: 'string'
+        password: 'string',
+        created: {
+            type: Date,
+            default: Date.now
+        }
     })),
     Account: mongoose.model('Account', new mongoose.Schema({
         userIdentifier: 'ObjectId',
+        // Identifies this account in the external system
         externalIdentifier: 'string',
         logOn: 'string',
-        password: 'string'
+        password: 'string',
+        created: {
+            type: Date,
+            default: Date.now
+        },
+        // Identifies, which system that account is for
+        source: 'string',
+        // Any additional metadata in a simplified form
+        tags: ['string']
     })),
     Session: mongoose.model('Session', new mongoose.Schema({
         accountIdentifier: 'ObjectId',
